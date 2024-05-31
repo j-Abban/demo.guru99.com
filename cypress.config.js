a7f2575487b3b5bc730be9fd40a0c14686752f3d
j-Abban <justice.abban@amalitech.org>
2024-05-31 15:41:46 +0000

    I have automated the 'Edit Customer' functionality

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 1,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000
   },
 });
